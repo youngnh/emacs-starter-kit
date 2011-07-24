a6d8351bdaf8c5e9a3d74e7cafe803faf35432a5
Nate Young <nyoung@revelytix.com>
2011-07-24 13:28:57 -0500

    enable upcase and downcase-region commands

--- n/init.el
+++ n/init.el
@@ -1,7 +1,5 @@
-(add-to-list 'load-path (concat user-specific-dir "/solarized"))
-
-(require 'color-theme-solarized)
-(color-theme-solarized-dark)
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
 
 ;; don't wrap lines
 (setq-default truncate-lines t)
