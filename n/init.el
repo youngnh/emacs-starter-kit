f4ebca115af86e4ac0123e5fe5464d386134754f
Nate Young <nyoung@revelytix.com>
2011-07-27 10:27:47 -0500

    bringing the wombat theme up to speed

---
+++ n/init.el
@@ -0,0 +1,18 @@
+(color-theme-wombat)
+
+;; enable these magnificently useful commands
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+
+;; column numbers
+(column-number-mode 1)
+
+;; don't wrap lines
+(setq-default truncate-lines t)
+
+;; so I can more easily tell where I am in a file
+(when window-system
+  (blink-cursor-mode 1))
+
+;; when marking TODOs done, also note the time
+(setq org-log-done 'time)
